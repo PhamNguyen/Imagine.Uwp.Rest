d8eb8f9e5f33b3658e92d7234812e48ea883bd47
PhamNguyen <pham.nguyen@hotmail.com>
2016-12-28 17:18:31 +0700

    SetQuery

---
+++ RestUWP/Attributes/SignAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace JsonUWP.Attributes
+{
+    public class SignAttribute : Attribute
+    {
+        public string Name { get; set; }
+
+        public Type Type { get; set; }
+
+        public bool IsList { get; set; }
+
+
+        public SignAttribute(string name)
+        {
+            Name = name;
+        }
+    }
+}
