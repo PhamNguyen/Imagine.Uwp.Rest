a49faee7e056c1f4c6d0507d1b92d806baaa1676
PhamNguyen <pham.nguyen@hotmail.com>
2016-12-31 14:43:41 +0700

    Change namespace Imagine.Uwp.Rest

--- RestUWP/RestClient.cs
+++ RestUWP/RestClient.cs
@@ -8,7 +8,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Imagine.Rest.UWP
+namespace Imagine.Uwp.Rest
 {
     public enum HttpScheme
     {
--- RestUWP/RestConfiguration.cs
+++ RestUWP/RestConfiguration.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Imagine.Rest.UWP
+namespace Imagine.Uwp.Rest
 {
     public class RestConfiguration
     {
