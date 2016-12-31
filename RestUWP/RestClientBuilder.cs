4d835953d6fa38655b83121e672c0ea82f8a5d92
PhamNguyen <pham.nguyen@hotmail.com>
2016-12-31 12:37:48 +0700

    Imagine.Rest.UWP

--- RestUWP/RestClientBuilder.cs
+++ RestUWP/RestClientBuilder.cs
@@ -5,7 +5,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RestUWP
+namespace Imagine.Rest.UWP
 {
     public class RestClientBuilder : IRestClientBuilder
     {
